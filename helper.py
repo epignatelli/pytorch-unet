2d6740dddc1f217dda526b6007ca15e3b0b2a18c
epignatelli <edu.pignatelli@gmail.com>
2020-07-01 16:47:10 +0100

    to pre-saved data

--- helper.py
+++ helper.py
@@ -1,22 +1,40 @@
+import itertools
+from functools import reduce
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+import os
+
+
+def save(folder, images, masks):
+    os.makedirs(folder, exist_ok=True)
+
+    for i in range(len(images)):
+        im = Image.fromarray(images[i])
+        mask = Image.fromarray(masks_to_colorimg(masks[i]))
+        im.save(os.path.join(folder, "{}_image.jpg".format(i)))
+        mask.save(os.path.join(folder, "{}_mask.jpg".format(i)))
+
+    return
+
 
 def plot_img_array(img_array, ncol=3):
     nrow = len(img_array) // ncol
 
-    f, plots = plt.subplots(nrow, ncol, sharex='all', sharey='all', figsize=(ncol * 4, nrow * 4))
+    f, plots = plt.subplots(nrow, ncol, sharex='all',
+                            sharey='all', figsize=(ncol * 4, nrow * 4))
 
     for i in range(len(img_array)):
         plots[i // ncol, i % ncol]
         plots[i // ncol, i % ncol].imshow(img_array[i])
 
-from functools import reduce
+
 def plot_side_by_side(img_arrays):
-    flatten_list = reduce(lambda x,y: x+y, zip(*img_arrays))
+    flatten_list = reduce(lambda x, y: x+y, zip(*img_arrays))
 
     plot_img_array(np.array(flatten_list), ncol=len(img_arrays))
 
-import itertools
+
 def plot_errors(results_dict, title):
     markers = itertools.cycle(('+', 'x', 'o'))
 
@@ -30,17 +48,20 @@ def plot_errors(results_dict, title):
 
     plt.show()
 
-def masks_to_colorimg(masks):
-    colors = np.asarray([(201, 58, 64), (242, 207, 1), (0, 152, 75), (101, 172, 228),(56, 34, 132), (160, 194, 56)])
 
-    colorimg = np.ones((masks.shape[1], masks.shape[2], 3), dtype=np.float32) * 255
+def masks_to_colorimg(masks):
+    colors = np.asarray([(201, 58, 64), (242, 207, 1), (0, 152, 75),
+                         (101, 172, 228), (56, 34, 132), (160, 194, 56)])
+
+    colorimg = np.ones(
+        (masks.shape[1], masks.shape[2], 3), dtype=np.float32) * 255
     channels, height, width = masks.shape
 
     for y in range(height):
         for x in range(width):
-            selected_colors = colors[masks[:,y,x] > 0.5]
+            selected_colors = colors[masks[:, y, x] > 0.5]
 
             if len(selected_colors) > 0:
-                colorimg[y,x,:] = np.mean(selected_colors, axis=0)
+                colorimg[y, x, :] = np.mean(selected_colors, axis=0)
 
     return colorimg.astype(np.uint8)
